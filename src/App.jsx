cdb4ecf3f2e71c9c3d9b9e99695f391e35828d07
Dipanshu Rawat <dv451197@gmail.com>
2025-11-05 12:21:57 +0530

    chore(remove): comments

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,6 @@ function App() {
           }}
         >
           <LeftSideVideo
-            // src="https://videos.pexels.com/video-files/7680438/7680438-hd_1920_1080_25fps.mp4"
             src="https://www.pexels.com/download/video/7680438/"
             label="I am a Designer"
           />
@@ -31,7 +30,6 @@ function App() {
         >
           <RightSideVideo
             src="https://www.pexels.com/download/video/9853180/"
-            // src="https://videos.pexels.com/video-files/3389913/3389913-hd_1920_1080_25fps.mp4"
             label="I need a Designer"
           />
         </Box>
